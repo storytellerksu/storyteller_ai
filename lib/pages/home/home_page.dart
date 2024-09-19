287971e62a362f95e6a3bd99b01e383fc6eb01da
Jung-en <110263150+Jung-en@users.noreply.github.com>
2024-09-19 03:41:26 -0400

    Created Bottom Navbar
    Bottom navbar and working on functionality to switch between pages

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,18 +1,40 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:storytellerai/pages/home/menu_page.dart';
+import 'package:storytellerai/pages/home/settings_page.dart';
+import 'package:storytellerai/pages/home/story_page.dart';
 import 'package:storytellerai/services/auth.dart';
 
-class Home_Page extends StatelessWidget {
+class Home_Page extends StatefulWidget {
   Home_Page({super.key});
 
+  @override
+  State<Home_Page> createState() => _Home_PageState();
+}
+
+class _Home_PageState extends State<Home_Page> {
   final AuthorizationService auth = AuthorizationService();
+
+  int currentPage = 0;
+
+  void changePage(index) {
+    setState(() {
+      currentPage = index;
+    });
+  }
+
+  final List pageList = [
+    Menu_Page(),
+    Story_Page(),
+    Settings_Page(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
-      appBar: AppBar(
+      /*appBar: AppBar(
         title: Text("Home Page"),
         backgroundColor: Colors.blue[300],
         actions: <Widget>[
@@ -23,7 +45,26 @@ class Home_Page extends StatelessWidget {
             icon: Icon(Icons.logout),
           ),
         ],
+      ),*/
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: changePage,
+        currentIndex: currentPage,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Menu",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.sailing),
+            label: "Create",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
       ),
+      body: pageList[currentPage],
     );
   }
 }
