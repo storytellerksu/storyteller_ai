4f5397a385bc1c3cf74d51f13f8b8b2ff7a4677e
Jung-en <110263150+Jung-en@users.noreply.github.com>
2024-09-19 04:01:40 -0400

    implemented logout button
    added logout button to settings page for testing purposes

--- lib/pages/home/settings_page.dart
+++ lib/pages/home/settings_page.dart
@@ -1,14 +1,27 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:storytellerai/services/auth.dart';
 
 class Settings_Page extends StatelessWidget {
-  const Settings_Page({super.key});
+  Settings_Page({super.key});
+
+  final AuthorizationService auth = AuthorizationService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("SETTTINNGS", style: TextStyle(fontSize: 100)),
+      body: Column(
+        children: [
+          Text("SETTTINNGS", style: TextStyle(fontSize: 100)),
+          IconButton(
+            onPressed: () async {
+              await auth.logOut();
+            },
+            icon: Icon(Icons.logout),
+          )
+        ],
+      ),
     );
   }
 }
