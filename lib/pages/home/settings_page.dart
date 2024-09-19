fc5270259bd174b3a6adfa6afa4ac8e24ee48d33
Jung-en <110263150+Jung-en@users.noreply.github.com>
2024-09-19 03:40:37 -0400

    Created Settings Page

---
+++ lib/pages/home/settings_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Settings_Page extends StatelessWidget {
+  const Settings_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
