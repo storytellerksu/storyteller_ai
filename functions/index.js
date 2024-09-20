677d767ff072ca48899884fc46650299581d7263
Jung-en <110263150+Jung-en@users.noreply.github.com>
2024-09-19 23:09:23 -0400

    Installed dependencies
    Installed Firestore and Cloud Function Dependencies

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
