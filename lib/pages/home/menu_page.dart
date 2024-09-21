9421815427831ed755042a72fdadc39f3f5c74ee
Jung-en <110263150+Jung-en@users.noreply.github.com>
2024-09-21 18:03:37 -0400

    Menu displays username
    Menu prints username the user to the menu screen

--- lib/pages/home/menu_page.dart
+++ lib/pages/home/menu_page.dart
@@ -1,9 +1,38 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:storytellerai/services/user_services.dart';
+import 'package:storytellerai/services/database.dart';
+import 'package:storytellerai/services/myUser.dart';
 
-class Menu_Page extends StatelessWidget {
-  const Menu_Page({super.key});
+class Menu_Page extends StatefulWidget {
+  Menu_Page({super.key});
+
+  @override
+  State<Menu_Page> createState() => _Menu_PageState();
+}
+
+class _Menu_PageState extends State<Menu_Page> {
+  final UserServices auth = UserServices();
+  String username = "";
+
+  @override
+  void initState() {
+    super.initState();
+    getUsername();
+  }
+
+  Future<void> getUsername() async {
+    try {
+      String fetchedUsername = await auth.getUsername();
+      setState(() {
+        username = fetchedUsername;
+      });
+    } catch (error) {
+      print('Error fetching username: $error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +40,7 @@ class Menu_Page extends StatelessWidget {
       canPop: false,
       child: Scaffold(
         backgroundColor: const Color.fromARGB(255, 174, 233, 227),
-        body: Text("MEnu SCREEN with top stories?",
-            style: TextStyle(fontSize: 90)),
+        body: Text("Welcome $username", style: TextStyle(fontSize: 90)),
       ),
     );
   }
