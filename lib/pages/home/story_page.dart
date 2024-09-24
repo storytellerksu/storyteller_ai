62147fea62446a2e61d4f40d7b953297296100f9
Jung-en <110263150+Jung-en@users.noreply.github.com>
2024-09-24 04:14:35 -0400

    Added cloud function method

--- lib/pages/home/story_page.dart
+++ lib/pages/home/story_page.dart
@@ -1,9 +1,38 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
 
-class Story_Page extends StatelessWidget {
+class Story_Page extends StatefulWidget {
   const Story_Page({super.key});
+
+  @override
+  State<Story_Page> createState() => _Story_PageState();
+}
+
+class _Story_PageState extends State<Story_Page> {
+  String storyText = "";
+
+  getStory() async {
+    print("step 1");
+    final result =
+        await FirebaseFunctions.instance.httpsCallable("generateText").call(
+      {
+        "text": "text",
+        "push": true,
+      },
+    );
+    print("step 2");
+    if (result.data is String) {
+      print("step 3");
+      setState(() {
+        storyText = result.data as String;
+      });
+      print("step 4");
+    } else {
+      throw Exception("Expected a String but got ${result.data.runtimeType}");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +43,15 @@ class Story_Page extends StatelessWidget {
         body: Column(
           children: [
             SizedBox(height: 30),
-            Image.asset(
-              "assets/storypage_mock.png",
+            Center(
+              child: ElevatedButton(
+                onPressed: () async {
+                  await getStory();
+                },
+                child: Text("Press to Generate Story!"),
+              ),
             ),
-            Text("fake page btw"),
+            Text(storyText),
           ],
         ),
       ),
