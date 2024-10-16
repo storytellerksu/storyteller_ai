7ed5870ecbaec7e3fcc419fb6fbfce0794f958ad
Jung-en <110263150+Jung-en@users.noreply.github.com>
2024-10-16 04:18:11 -0400

    Added API calls for Image Generation
    Created callable cloud function that calls OpenAI for Dall-e Image Generation

--- functions/main.py
+++ functions/main.py
@@ -15,10 +15,16 @@ from dotenv import load_dotenv
 
 # Any allows it to accept any data type
 from typing import Any
+import re
 
+# initializes cloud function, did not return api call without this
 initialize_app()
 
-# creates callable function
+# gets api key from env
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+# creates callable function for text generation
 @https_fn.on_call()
 def generateText(req: https_fn.CallableRequest) -> Any:
 # Message text passed from the client.
@@ -31,15 +37,26 @@ def generateText(req: https_fn.CallableRequest) -> Any:
     storyText = generate(inputText)
     return {"text": storyText}
 
-load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+# creates callable function for image generation
+@https_fn.on_call()
+def generateImage(req: https_fn.CallableRequest) -> Any:
+# Message text passed from the client.
+    inputText = req.data["text"]
+# Authentication / user information is automatically added to the request.
+    uid = req.auth.uid
+#    name = req.auth.token.get("name", "")
+#    picture = req.auth.token.get("picture", "")
+#    email = req.auth.token.get("email", "")
+    imageURL = generate2(inputText)
+    return {"text": imageURL}
 
+# makes api call to openai for text generation
 def generate(inputText):
     response = client.chat.completions.create(model="gpt-4o-mini",
     messages=[
       {
         "role": "system",
-        "content": "You are a creative storyteller who tells very short stories. Write a short story that is strictly less than 150 words."
+        "content": "You are a creative storyteller who tells short stories. Write a short story that is strictly less than 150 words. After each sentence add || to denote the end of a sentence."
       },
       {
         "role": "user",
@@ -51,9 +68,17 @@ def generate(inputText):
     top_p=1)
     return response.choices[0].message.content
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+# makes api call to openai for image generation using Dalle-3
+def generate2(inputText):
+  try:
+    response2 = client.images.generate(
+    model="dall-e-2",
+              prompt=inputText,
+              size="1024x1024",
+              quality="standard",
+              n=1,
+          )
+    return response2.data[0].url
+  except Exception as e:
+      print(f"Error generating image for sentence '{inputText}': {str(e)}")
+      return "https://dummyimage.com/300"
